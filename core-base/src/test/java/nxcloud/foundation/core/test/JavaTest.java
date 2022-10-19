88c3bf74a383f4e615b3adf65ef49b641ab4ef0a
Sunshow <sunshow@gmail.com>
2022-10-19 15:14:46 +0800

    搭建项目

---
+++ core-base/src/test/java/nxcloud/foundation/core/test/JavaTest.java
@@ -0,0 +1,13 @@
+package nxcloud.foundation.core.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JavaTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
---
+++ core-base/src/test/kotlin/nxcloud/foundation/core/test/KotlinTest.kt
@@ -0,0 +1,13 @@
+package nxcloud.foundation.core.test
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+internal class KotlinTest {
+
+    @Test
+    fun test() {
+        assertTrue { true }
+    }
+
+}
