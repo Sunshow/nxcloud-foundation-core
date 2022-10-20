d43a875f38928e7d8d10ce5f423dfd4cdf353c9a
Sunshow <sunshow@gmail.com>
2022-10-20 10:55:17 +0800

    use gradle platform dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    `java-library`
+}
+
 buildscript {
     dependencies {
         classpath(libs.gradlePlugin.kotlin)
         classpath(libs.gradlePlugin.springboot)
-        classpath(libs.gradlePlugin.dependency.management)
     }
 
     repositories {
@@ -48,10 +51,8 @@ allprojects {
 subprojects {
     val project = this@subprojects
 
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "java-library")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
 
     tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.toString()
@@ -73,12 +74,6 @@ subprojects {
     }
 
     val testJavaVersion = System.getProperty("test.java.version", "11").toInt()
-
-    val testRuntimeOnly: Configuration by configurations.getting
-    dependencies {
-        testRuntimeOnly(rootProject.libs.junit.jupiter.engine)
-        testRuntimeOnly(rootProject.libs.junit.vintage.engine)
-    }
 
     tasks.withType<Test> {
         useJUnitPlatform()
@@ -104,107 +99,17 @@ subprojects {
         targetCompatibility = JavaVersion.VERSION_1_8.toString()
     }
 
-    dependencyManagement {
-        resolutionStrategy {
-            cacheChangingModulesFor(0, "seconds")
-            cacheDynamicVersionsFor(0, "seconds")
-        }
-
-        imports {
-            mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-        }
-
-        dependencies {
-            dependency("org.slf4j:slf4j-api:${slf4jVersion}")
-        }
+    dependencies {
+        implementation(platform(rootProject.libs.bom.springboot))
+        implementation("org.slf4j:slf4j-api")
+        testImplementation("org.jetbrains.kotlin:kotlin-test")
+        testImplementation("org.junit.jupiter:junit-jupiter-api")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+        testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
     }
+
 }
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
-
-/*
-plugins {
-    id 'org.jetbrains.kotlin.jvm' version '1.7.10' apply false
-    id 'org.jetbrains.kotlin.plugin.lombok' version '1.7.10' apply false
-    id 'io.freefair.lombok' version '6.5.1' apply false
-    id 'org.springframework.boot' version '2.7.4' apply false
-    id 'org.jetbrains.kotlin.plugin.spring' version '1.7.10' apply false
-}
-
-ext {
-    slf4jVersion = '2.0.3'
-}
-
-subprojects {
-    apply plugin: 'idea'
-    apply plugin: 'java-library'
-    apply plugin: 'io.spring.dependency-management'
-    apply plugin: 'org.jetbrains.kotlin.jvm'
-    apply plugin: 'org.jetbrains.kotlin.plugin.spring'
-    apply plugin: 'org.jetbrains.kotlin.plugin.lombok'
-    apply plugin: 'io.freefair.lombok'
-
-    repositories {
-        mavenCentral()
-    }
-
-    apply plugin: 'io.spring.dependency-management'
-    apply plugin: 'idea'
-
-    dependencyManagement {
-        resolutionStrategy {
-            cacheChangingModulesFor 0, 'seconds'
-            cacheDynamicVersionsFor 0, 'seconds'
-        }
-
-        imports {
-            mavenBom SpringBootPlugin.BOM_COORDINATES
-        }
-
-        dependencies {
-            dependency "org.slf4j:slf4j-api:${slf4jVersion}"
-        }
-    }
-
-    dependencies {
-        testImplementation 'org.jetbrains.kotlin:kotlin-test'
-        implementation 'org.jetbrains.kotlin:kotlin-stdlib'
-        implementation 'org.slf4j:slf4j-api'
-    }
-
-    test {
-        useJUnitPlatform()
-    }
-
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-
-    tasks.compileKotlin {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
-    }
-
-    tasks.compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
-    }
-
-    tasks.withType(JavaCompile) {
-        options.encoding = 'UTF-8'
-    }
-
-    tasks.withType(GenerateModuleMetadata) {
-        enabled = false
-    }
-
-    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
-        kotlinOptions {
-            freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
-        }
-    }
-}
-*/
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "nxcloud-foundation-core"
 include(":core-base")
 include(":core-bean")
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
