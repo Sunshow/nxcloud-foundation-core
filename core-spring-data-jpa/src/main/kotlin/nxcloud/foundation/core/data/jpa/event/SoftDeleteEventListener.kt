2a657551be1e019bfc73a3054eb1715a4306cc75
Sunshow <sunshow@gmail.com>
2022-11-04 13:28:34 +0800

    增加软删除拦截日志

--- core-spring-data-jpa/src/main/kotlin/nxcloud/foundation/core/data/jpa/event/SoftDeleteEventListener.kt
+++ core-spring-data-jpa/src/main/kotlin/nxcloud/foundation/core/data/jpa/event/SoftDeleteEventListener.kt
@@ -1,18 +1,20 @@
 package nxcloud.foundation.core.data.jpa.event
 
+import mu.KotlinLogging
 import nxcloud.foundation.core.data.jpa.entity.SoftDeleteJpaEntity
 import org.hibernate.event.spi.PreDeleteEvent
 import org.hibernate.event.spi.PreDeleteEventListener
 
 class SoftDeleteEventListener : PreDeleteEventListener {
+
+    private val logger = KotlinLogging.logger {}
+
     override fun onPreDelete(event: PreDeleteEvent): Boolean {
         val entity = event.entity
 
         if (entity is SoftDeleteJpaEntity) {
-            //entity.deleted = System.currentTimeMillis()
-            // event.session.update(entity)
-//            event.session.flush()
             // 禁用物理删除
+            logger.error { "当前实体已启用软删除, 禁用物理删除, entity=$entity" }
             return true
         }
 
