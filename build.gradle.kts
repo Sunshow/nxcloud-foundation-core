3652da0d4005fb2d4083d610a64811608f9140ad
Sunshow <sunshow@gmail.com>
2022-11-10 14:49:51 +0800

    升级版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 allprojects {
     group = "nxcloud.foundation"
-    version = "0.1.0-SNAPSHOT"
+    version = "0.2.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
@@ -131,7 +131,7 @@ subprojects {
     publishing {
 
         // 发布 release
-        // version = "1.0"
+        // version = "0.2.0"
 
         val sourcesJar by tasks.registering(Jar::class) {
             archiveClassifier.set("sources")
