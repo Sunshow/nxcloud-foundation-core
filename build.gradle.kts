f97d571a728a6bad30e61c85f7c65e83a4ef684d
Sunshow <sunshow@gmail.com>
2023-08-14 14:19:00 +0800

    已指定有效ID (> 0) 则不生成

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,7 +135,7 @@ subprojects {
     publishing {
 
         // 发布 release
-        version = "0.5.2"
+        version = "0.5.3"
 
         val sourcesJar by tasks.registering(Jar::class) {
             archiveClassifier.set("sources")
