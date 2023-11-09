88c479580905713777c501ab5fca0adaa9add873
Sunshow <sunshow@gmail.com>
2023-11-09 09:51:37 +0800

    修改jpa依赖为api

--- core-spring-boot-starter-data-jpa/build.gradle.kts
+++ core-spring-boot-starter-data-jpa/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     api(project(":core-spring-boot-starter"))
     api(project(":core-spring-data-jpa"))
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
 }
