e1852c473617d9a83dd46d2e1633cd3967f5c4ff
Sunshow <sunshow@gmail.com>
2024-06-26 23:41:36 +0800

    发布 0.5.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -136,7 +136,7 @@ subprojects {
     publishing {
 
         // 发布 release
-        version = "0.5.6"
+        version = "0.5.7"
 
         val sourcesJar by tasks.registering(Jar::class) {
             archiveClassifier.set("sources")
