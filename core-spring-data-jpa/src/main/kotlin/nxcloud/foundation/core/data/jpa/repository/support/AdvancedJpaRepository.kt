858b8caf7219f507bfda1a18f6339ce4844b43a2
Sunshow <sunshow@gmail.com>
2024-09-27 17:46:36 +0800

    实现自定义的 findById 和 existsById

--- core-spring-data-jpa/src/main/kotlin/nxcloud/foundation/core/data/jpa/repository/support/AdvancedJpaRepository.kt
+++ core-spring-data-jpa/src/main/kotlin/nxcloud/foundation/core/data/jpa/repository/support/AdvancedJpaRepository.kt
@@ -3,6 +3,7 @@ package nxcloud.foundation.core.data.jpa.repository.support
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.persistence.EntityManager
 import jakarta.persistence.TypedQuery
+import jakarta.persistence.criteria.Root
 import nxcloud.foundation.core.data.jpa.entity.DeletedField
 import nxcloud.foundation.core.data.support.annotation.EnableSoftDelete
 import org.springframework.core.annotation.AnnotationUtils
@@ -11,19 +12,22 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.domain.Specification
+import org.springframework.data.jpa.provider.PersistenceProvider
+import org.springframework.data.jpa.repository.support.CrudMethodMetadata
 import org.springframework.data.jpa.repository.support.JpaEntityInformation
 import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository
 import org.springframework.data.repository.query.FluentQuery
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.util.Assert
 import java.util.*
 import java.util.function.Function
 
 @Repository
 @Transactional(readOnly = true)
-open class AdvancedJpaRepository<T : Any, ID : Any>(
-    entityInformation: JpaEntityInformation<T, *>,
+class AdvancedJpaRepository<T : Any, ID : Any>(
+    private val entityInformation: JpaEntityInformation<T, *>,
     private val entityManager: EntityManager,
 ) : SimpleJpaRepository<T, ID>(entityInformation, entityManager) {
 
@@ -38,8 +42,20 @@ open class AdvancedJpaRepository<T : Any, ID : Any>(
         em,
     )
 
+    companion object {
+        private const val ID_MUST_NOT_BE_NULL = "The given id must not be null"
+    }
+
     private val enableSoftDelete: EnableSoftDelete? by lazy {
         AnnotationUtils.findAnnotation(entityInformation.javaType, EnableSoftDelete::class.java)
+    }
+
+    private val provider: PersistenceProvider by lazy {
+        PersistenceProvider.fromEntityManager(entityManager)
+    }
+
+    private val metadata: CrudMethodMetadata? by lazy {
+        super.getRepositoryMethodMetadata()
     }
 
     /**
@@ -52,8 +68,39 @@ open class AdvancedJpaRepository<T : Any, ID : Any>(
     /**
      * 暴露实体类型
      */
-    override fun getDomainClass(): Class<T> {
+    public override fun getDomainClass(): Class<T> {
         return super.getDomainClass()
+    }
+
+    private fun applyComment(metadata: CrudMethodMetadata, consumer: (String, Any) -> Unit) {
+        metadata.comment
+            ?.takeIf {
+                provider.commentHintKey != null
+            }
+            ?.also {
+                provider.getCommentHintValue(it)
+                    ?.apply {
+                        consumer(provider.commentHintKey!!, this)
+                    }
+            }
+    }
+
+    private fun getHints(): Map<String, Any> {
+        val hints = mutableMapOf<String, Any>()
+
+        queryHints.withFetchGraphs(entityManager)
+            .forEach { t, u ->
+                hints[t] = u
+            }
+
+        metadata
+            ?.also {
+                applyComment(it) { key, value ->
+                    hints[key] = value
+                }
+            }
+
+        return hints
     }
 
     override fun <S : T> getCountQuery(spec: Specification<S>?, domainClass: Class<S>): TypedQuery<Long> {
@@ -129,11 +176,33 @@ open class AdvancedJpaRepository<T : Any, ID : Any>(
     }
 
     override fun existsById(id: ID): Boolean {
-        return super.existsById(id)
+        return this.findById(id).isPresent
     }
 
     override fun findById(id: ID): Optional<T> {
-        return super.findById(id)
+        Assert.notNull(id, ID_MUST_NOT_BE_NULL)
+
+        val cb = entityManager.criteriaBuilder
+        val cq = cb.createQuery(domainClass)
+        val root: Root<T> = cq.from(domainClass)
+
+        val idPredicate = cb.equal(root.get<Any>(entityInformation.idAttribute.name), id)
+        cq.where(idPredicate)
+
+        val query = entityManager.createQuery(cq)
+        
+        metadata
+            ?.lockModeType
+            ?.also {
+                query.setLockMode(it)
+            }
+
+        getHints()
+            .forEach { (k, v) ->
+                query.setHint(k, v)
+            }
+
+        return Optional.ofNullable(query.singleResult)
     }
 
     @Transactional
